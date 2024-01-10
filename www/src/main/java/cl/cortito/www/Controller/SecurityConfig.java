1de9a1855b43cc15fd09fcbe1951ca249adb2a0a
Fernando Michea <fmicheam@gmail.com>
2024-01-10 14:40:20 -0300

    basic_auth

--- www/src/main/java/cl/cortito/www/Controller/SecurityConfig.java
+++ www/src/main/java/cl/cortito/www/Controller/SecurityConfig.java
@@ -2,12 +2,24 @@ package cl.cortito.www.Controller;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig   {
+    @Autowired
+    private UserDetailsService userDetailsService;
+   
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -16,10 +28,18 @@ public class SecurityConfig {
             // Configura las solicitudes para permitir todas las solicitudes sin autenticación.
             .authorizeHttpRequests((authorize) -> authorize
                 .requestMatchers("/usuario/login").permitAll()
+                .requestMatchers("/usuario").permitAll()
                 .anyRequest().authenticated())    
             // Aplica la autenticación HTTP básica con la configuración predeterminada.
             .httpBasic(withDefaults());
         // Construye y devuelve la cadena de filtros de seguridad configurada.
         return http.build();
     }
+
+    @Autowired 
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+         
+    }
+
 }
--- www/src/main/java/cl/cortito/www/Service/UsuarioService.java
+++ www/src/main/java/cl/cortito/www/Service/UsuarioService.java
@@ -1,21 +1,44 @@
 package cl.cortito.www.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
-import cl.cortito.www.Model.Persona;
 import cl.cortito.www.Model.Usuario;
+import cl.cortito.www.Model.Persona;
+
 import cl.cortito.www.Repository.UsuarioRepository;
-import java.util.Optional;
+
+import java.util.Collections;
 
 @Service
-public class UsuarioService {
+public class UsuarioService implements UserDetailsService {
     @Autowired
     UsuarioRepository usuarioRepository;
 
+    
     // crear usuario
-    public void crearUsuario(Usuario usuario){
+    public void crearUsuario(Usuario usuario) {
+        String contrasena = usuario.getContrasena();
+        String contrasenaEncriptada = new BCryptPasswordEncoder().encode(contrasena);
+        usuario.setContrasena(contrasenaEncriptada);
         usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String correo) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByCorreo(correo);
+        if (usuario == null) {
+            throw new UsernameNotFoundException("Usuario no encontrado con el correo: " + correo);
+        }
+
+        return new org.springframework.security.core.userdetails.User(usuario.getCorreo(), usuario.getContrasena(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+
     }
 
     // login usuario
@@ -32,5 +55,8 @@ public class UsuarioService {
         }
     }
 
-    
+   
+
+   
+
 }
